0f4dc5b764cf74fd9158a9b8134ad56048037e1f
Akshat Mishra <akshatmishra317@gmail.com>
2024-08-21 22:17:47 +0530

    day 33

---
+++ Greedy/all-unique-permutations-in-an-array.cpp
@@ -0,0 +1,12 @@
+    vector<vector<int>> uniquePerms(vector<int> &arr ,int n) {
+        vector<vector<int>> perm;
+        
+        sort(arr.begin(),arr.end());
+        
+        do{
+            perm.push_back(arr);
+        }
+        while(next_permutation(arr.begin(),arr.end()));
+        
+        return perm;
+    }
